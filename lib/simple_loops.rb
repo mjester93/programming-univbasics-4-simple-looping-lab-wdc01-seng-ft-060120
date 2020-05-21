c5769b640d82dd937cc8d7f80cfc26369c877199
Michael Jester <m.jester.93@gmail.com>
2020-05-21 20:18:37 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -5,25 +5,3 @@ def loop_message_five_times(message)
     puts message
     counter += 1
   end
-end
-
-def loop_message_n_times(message, n)
-  counter = 0
-  while counter < n do
-    puts message
-    counter += 1
-  end
-end
-
-def output_array(array)
-  for array.each do |n|
-    puts n
-  end
-end
-
-def return_string_array(array)
-  new_array = []
-  for array.each do |n|
-    new_array.push(n.to_s)
-  end
-end
